f05739b69a17c25a7922e629634dd96707093add
1x-coha <christopher.ohanesian@1x.tech>
2024-07-24 01:37:53 +0000

    finally reading data from logs file and displaying in a tab

--- app.py
+++ app.py
@@ -2,66 +2,50 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from dataclasses import dataclass
-from enum import Enum
-
 
 # Data sources
 data_dir = 'data/'
 projects = data_dir + 'projects.csv'
+logs = data_dir + 'logs.csv'
 
 @dataclass
 class Skill:
-    Uid: str
     Name: str
-    Libraries: dict
-    Created: str
-    Models: str
+    Date: str
+    Models: str 
+    Libraries: str 
 
 @dataclass
 class Library:
-    Uid: str
     Name: str
-    Skills: dict
+    Date: str
+    Skills: str
 
 @dataclass
 class Log:
-    Uid: str
     Name: str
-    FileName: str
+    Date: str
+    File: str
     Captions: int
-    FeaturizedFrames: int
-    Created: str
+    Featurized: int
 
-class SkillType(Enum):
-    GENERAL = 'General Skill'
-    UNIQUE = 'Unique Skill'
-    RECOVERY = 'Recovery Skill'
-
-
+# Sidebar
 with st.sidebar:
     'Hello USER!'
     with (st.expander('My settings')):
         'Settings'
-    ''
-    'Here are some links to other tools:'
-    'Aiviz'
-    'Air Fryer'
-    'FastCaption'
-
+    'Some more stuff'
 
 # App title
 st.title('Pickle :cucumber:')
 
-
 # Library selection
 st.selectbox('Select a skill library',['My library'])
 
+# Tabs
+tab_1, tab_2, tab_3 = st.tabs(['Logs', 'Training', 'Testing'])
 
-# Skills dashboard
-skills = [
-    Skill(Uid: 'SKI-1', Name: 'Skill 1', Libraries: 'My Library', '', ''),
-    Skill(Uid: 'SKI-2', Name: 'Skill 2', Libraries: 'My Library', '', '')
-]
-
-for skill in skills:
-    st.write()
+# Logs tab
+with tab_1:
+    logs_data = pd.read_csv(logs)
+    st.dataframe(logs_data)
